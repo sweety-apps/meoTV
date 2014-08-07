e6f595932b71704e3b2c7220f3a962a245fb8371
yaonimingsanqian <529670074@qq.com>
2014-08-07 11:30:27 +0800

    去掉无用的包

--- yo/Pods/Pods-environment.h
+++ yo/Pods/Pods-environment.h
@@ -6,48 +6,6 @@
 // project.
 
 
-// AFNetworking
-#define COCOAPODS_POD_AVAILABLE_AFNetworking
-#define COCOAPODS_VERSION_MAJOR_AFNetworking 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking 1
-
-// AFNetworking/NSURLConnection
-#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
-#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 1
-
-// AFNetworking/NSURLSession
-#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
-#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 1
-
-// AFNetworking/Reachability
-#define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
-#define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 1
-
-// AFNetworking/Security
-#define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
-#define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 1
-
-// AFNetworking/Serialization
-#define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
-#define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 1
-
-// AFNetworking/UIKit
-#define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
-#define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
-
 // APAudioPlayer
 #define COCOAPODS_POD_AVAILABLE_APAudioPlayer
 #define COCOAPODS_VERSION_MAJOR_APAudioPlayer 0
@@ -84,12 +42,6 @@
 #define COCOAPODS_VERSION_MINOR_ASIHTTPRequest_S3 8
 #define COCOAPODS_VERSION_PATCH_ASIHTTPRequest_S3 2
 
-// Base64
-#define COCOAPODS_POD_AVAILABLE_Base64
-#define COCOAPODS_VERSION_MAJOR_Base64 1
-#define COCOAPODS_VERSION_MINOR_Base64 0
-#define COCOAPODS_VERSION_PATCH_Base64 1
-
 // JSONKit
 #define COCOAPODS_POD_AVAILABLE_JSONKit
 // This library does not follow semantic-versioning,
@@ -109,12 +61,6 @@
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
 
-// MD5Digest
-#define COCOAPODS_POD_AVAILABLE_MD5Digest
-#define COCOAPODS_VERSION_MAJOR_MD5Digest 0
-#define COCOAPODS_VERSION_MINOR_MD5Digest 1
-#define COCOAPODS_VERSION_PATCH_MD5Digest 0
-
 // Reachability
 #define COCOAPODS_POD_AVAILABLE_Reachability
 #define COCOAPODS_VERSION_MAJOR_Reachability 3
@@ -132,10 +78,4 @@
 #define COCOAPODS_VERSION_MAJOR_SWTableViewCell 0
 #define COCOAPODS_VERSION_MINOR_SWTableViewCell 3
 #define COCOAPODS_VERSION_PATCH_SWTableViewCell 0
-
-// iOS7Colors
-#define COCOAPODS_POD_AVAILABLE_iOS7Colors
-#define COCOAPODS_VERSION_MAJOR_iOS7Colors 2
-#define COCOAPODS_VERSION_MINOR_iOS7Colors 1
-#define COCOAPODS_VERSION_PATCH_iOS7Colors 0
 
